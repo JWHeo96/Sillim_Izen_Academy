807d7a3c0e9e6ae4769b09f300762faadffd7e2d
JWHeo96 <eidhk7@gmail.com>
2023-01-06 13:13:36 +0900

    230106_오전 시간

--- src/Repeat/forSample10_APIClass2.java
+++ src/Repeat/forSample10_APIClass2.java
@@ -3,7 +3,7 @@ package Repeat;
 import java.util.Scanner;
 import java.util.StringTokenizer;
 
-public class forSample10_APIClass {
+public class forSample10_APIClass2 {
 
 	public static void main(String[] args) {
 		
@@ -12,7 +12,7 @@ public class forSample10_APIClass {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("문자열을 입력해주세요.");
 		if(sc.hasNext())
-			s = sc.next(); // "홍길동/박수홍/하석/한강빈/안혜지/박이레/김수환/임서하"
+			s = sc.next(); // "홍길동 박수홍 하석.한강빈 안혜지 박이레.김수환 임서하"
 		System.out.println("String: " + s);
 		
 		StringTokenizer st = new StringTokenizer(s,".");
---
+++ src/Repeat/forSample10_APIClass3.java
@@ -0,0 +1,33 @@
+package Repeat;
+
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class forSample10_APIClass3 {
+
+	public static void main(String[] args) {
+		//replace: 대체 메소드
+		
+		String s = null;
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("문자열을 입력해주세요.");
+		if(sc.hasNext())
+			s = sc.next(); // "자바는.객체지향.언어.입니다.자바는.풍부한.API를.지원합니다."
+		System.out.println("String: " + s);
+
+		// 문제> 자바라는 단어를 Java로 변경
+		// "Java는.객체지향.언어.입니다.Java는.풍부한.API를.지원합니다."
+		String oldJava = "자바는.객체지향.언어.입니다.자바는.풍부한.API를.지원합니다.";
+		
+		String newJava = null;
+		StringTokenizer st = new StringTokenizer(oldJava, ".");
+		while(st.hasMoreTokens()) {
+			newJava = (st.nextToken()).replace("자바", "Java");
+			System.out.print(newJava + " ");
+		}
+		
+
+	}
+}
+
