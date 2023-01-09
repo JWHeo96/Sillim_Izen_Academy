dbeb25de85103573234e8f8d49a5df0651116472
JWHeo96 <eidhk7@gmail.com>
2023-01-09 14:40:26 +0900

    123

--- src/methodOverloading/overloadSample2_Operation2.java
+++ src/methodOverloading/overloadSample2_Operation2.java
@@ -30,7 +30,7 @@ public class overloadSample2_Operation2 {
 		while(sw) {
 			System.out.println("수행하고자 하는 연산을 선택해주세요.\n1. 덧셈\t2. 뺄셈\t3. 나눗셈(나머지)\t4. 종료");
 			int z = sc.nextInt();
-			if(z==4) break;
+			if(z==4) System.exit(0);
 			
 			System.out.println("첫번째 값을 입력해주세요");
 			int x = sc.nextInt();
