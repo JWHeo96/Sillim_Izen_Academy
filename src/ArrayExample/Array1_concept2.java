d1614ef674302456139a8d16690cec4396937afb
JWHeo96 <eidhk7@gmail.com>
2023-01-09 16:28:53 +0900

    123

--- src/ArrayExample/Array1_concept2.java
+++ src/ArrayExample/Array1_concept2.java
@@ -2,6 +2,7 @@ package ArrayExample;
 
 import java.util.Arrays;
 import java.util.Random;
+import java.util.Scanner;
 
 public class Array1_concept2 {
 
@@ -10,22 +11,40 @@ public class Array1_concept2 {
 		 * 난수를 발생하여 로또 복권을 추첨합니다.
 		 * 중복을 제거하여 로또를 출력해 보세요.
 		 */
-		
-		Random lotto = new Random(); 
-		int lottos[] = new int[6];
-		for(int i=0; i<lottos.length; i++) {
-			lottos[i] = lotto.nextInt(45)+1;
-			for(int j=0; j<i; j++) {
-				if(lottos[i] == lottos[j]) {
-					i--;
+		boolean sw = true;
+
+		while(sw) {
+			Scanner sc = new Scanner(System.in);
+			
+			System.out.println("로또 자리수를 입력해주세요.");
+			int n = sc.nextInt();
+			System.out.println("반복 횟수를 입력해주세요.");
+			int m = sc.nextInt();
+			
+			Random lotto = new Random(); 
+			int lottos[] = new int[n];
+			
+			for(int x=0; x<m; x++) {
+				for(int i=0; i<lottos.length; i++) {
+					lottos[i] = lotto.nextInt(45)+1;
+					for(int j=0; j<i; j++) {
+						if(lottos[i] == lottos[j]) {
+							i--;
+						}
+					}
+				}
+				System.out.println((x+1) + "번: " + Arrays.toString(lottos));
+			}
+			
+			if(sw) {
+				System.out.println("\n난수를 계속 생성하겠습니까?(y/n)");
+				String str = sc.next();
+				if(str.equals("y")) {
+					continue;
+				} else {
+					sw=false;
 				}
 			}
-			System.out.println((i+1) +"번 숫자:" + lottos[i]);
 		}
-		System.out.println(Arrays.toString(lottos));
-		
-		
-		
 	} // main end
-
 } // class end
