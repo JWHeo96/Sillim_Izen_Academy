259fc575bcdf10467b1b91ce2cceca988ca4e39d
JWHeo96 <eidhk7@gmail.com>
2023-01-12 15:55:26 +0900

    콤마 표시

--- src/Company/Main_salaryCalculator.java
+++ src/Company/Main_salaryCalculator.java
@@ -6,18 +6,17 @@ public class Main_salaryCalculator extends Constructor_employee {
 
 	public static void main(String[] args) {
 		
-		DecimalFormat decFormat = new DecimalFormat("###,###");
+
 		
 		Overr_inputInfo td = new Overr_inputInfo();
 		td.inputInfo();
 		
 		Overr_Calculator calc = new Overr_Calculator();
 		int sum = calc.add(salary, commition);
+		
+		DecimalFormat decFormat = new DecimalFormat("###,###");
 		Integer.toString(sum);
-		
-
 		String result = decFormat.format(sum);
-		
 		
 		System.out.println(name + "님의 부서는 " + dept + 
 				"이고,\n연봉과 수당을 합친 총 금액은 " + result + "원 입니다.");
