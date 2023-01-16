2f614e149940fbe6f1d4e5b0f9c03ad0f8d1bbac
JWHeo96 <eidhk7@gmail.com>
2023-01-16 13:19:41 +0900

    SQL문
    CRUD

---
+++ src/oracleDBTest/memberSelect.java
@@ -0,0 +1,72 @@
+package oracleDBTest;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class memberSelect { 
+
+	private static Connection conn;
+	private static PreparedStatement pstmt;
+	private static ResultSet rs;
+
+//회원관리 조회시스템 개발
+
+	public static void main(String[] args) {
+		
+		// 1. 드라이버 로딩 및 연결정보 설정
+		String driver = "oracle.jdbc.OracleDriver";
+		String url = "jdbc:oracle:thin:@localhost:1521:xe"; // fullVersion => orcl
+		String userid = "heo";
+		String userpwd = "1234";
+		
+		try {
+			Class.forName(driver);
+			System.out.println("Oracle Driver Loading Successed!!!");
+			
+			try {
+				conn = DriverManager.getConnection(url, userid, userpwd);
+				System.out.println("Connection is Successed!!!");
+			} catch (SQLException e) {
+				e.printStackTrace();
+				System.out.println("url, userid, userpwd가 잘못되었습니다.");
+			}
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		
+		// 2. 조회 프로그램 작성
+		String sql = "select * from member order by id desc";
+		
+		try {
+			pstmt = conn.prepareStatement(sql);
+			
+			rs = pstmt.executeQuery();
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+			System.out.println("SQL 문장이 잘못되었습니다.!!!");
+		}
+		
+		System.out.println("=================================="
+				+ "===");
+		System.out.println("번호\t이름\t키\t몸무게\t나이");
+		System.out.println("=====================================");
+		// 3. 결과 출력 부문
+		try {
+			while(rs.next()) {
+				String id = rs.getString(1); //(1)
+				String name = rs.getString(2); //(2)
+				double height = rs.getDouble(3); //(3)
+				double weight = rs.getDouble("weight"); //(4)
+				int age = rs.getInt("age"); //(5)
+				
+				System.out.println(id + "\t" + name + "\t" + height + "\t" + weight +  "\t" + age);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+}
--- src/oracleDBTest/oracle_member.sql
+++ src/oracleDBTest/oracle_member.sql
@@ -1,10 +1,10 @@
 // 테이블 생성
 create table member (
-	id varchar2(10) primary key,
-	name varchar2(50),
-	height number(5,2),
-	weight number(5,2),
-	age number(3)
+	id varchar2(10) primary key, --이 테이블의 기준키, 유일성을 사용(중복 데이터 거부)
+	name varchar2(30) not null, --not null, 반드시 데이터를 입력하시오.
+	height number(5, 2) default 0.00, -- (5, 2), 총 5자리 중 소수점 아래 2자리까지 허용
+	weight number(5, 2) default 0.00,
+	age number(3) default 0
 );
 
 // member라는 테이블에 값 입력
@@ -19,3 +19,6 @@ select * from member;
 
 // 데이터 저장
 commit
+
+// 테이블 삭제
+drop table member;
