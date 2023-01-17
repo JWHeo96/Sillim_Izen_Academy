33e66cf81e6b35598426b71eac08bd57c5e2fe06
JWHeo96 <eidhk7@gmail.com>
2023-01-17 10:44:32 +0900

    230117

--- src/executeQuery/executeQuerys.java
+++ src/executeQuery/executeQuerys.java
@@ -9,6 +9,7 @@ public class executeQuerys {
 		
 		String driver = null;
 		String url = null;
+		boolean sw = true;
 		
 		Scanner sc = new Scanner(System.in);
 		System.out.println("연결하고자 하는 SQL을 입력해주세요\n(oracle/mysql)");
@@ -23,13 +24,17 @@ public class executeQuerys {
 		
 		CrudMatrix crud = new CrudMatrix();
 		crud.insertInfo(driver, url);
-		System.out.println("원하는 작업을 선택해주세요.\n1:생성\t2:조회\t3:수정\t4:삭제");
-		int numb = sc.nextInt();
-		switch(numb) {
-			case 1: crud.CreateTable(); break;
-			case 2: crud.ReadTable(); break;
-			case 3: crud.UpdateTable(); break;
-			case 4: crud.DeleteTable(); break;
+		
+		while(sw) {
+			System.out.println("원하는 작업을 선택해주세요.\n1:생성\t2:조회\t3:수정\t4:삭제\5:종료");
+			int numb = sc.nextInt();
+			switch(numb) {
+				case 1: crud.CreateTable(); break;
+				case 2: crud.ReadTable(); break;
+				case 3: crud.UpdateTable(); break;
+				case 4: crud.DeleteTable(); break;
+				case 5: sw = false; break;
+			}
 		}
 		
 	}
